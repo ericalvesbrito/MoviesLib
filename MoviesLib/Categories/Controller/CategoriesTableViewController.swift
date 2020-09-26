60c3e24e262d910d889334e53f36cb2eb8955e7f
Eric Brito <eric_brito@msn.com>
2020-09-26 15:23:10 -0300

    Finalizando CategoriesViewController

--- MoviesLib/Categories/Controller/CategoriesTableViewController.swift
+++ MoviesLib/Categories/Controller/CategoriesTableViewController.swift
@@ -31,10 +31,33 @@ class CategoriesTableViewController: UITableViewController {
     
     // MARK: - IBActions
     @IBAction func add(_ sender: UIBarButtonItem) {
-        
+        showCategoryAlert()
     }
     
     // MARK: - Methods
+    private func showCategoryAlert(for category: Category? = nil) {
+        let title = category == nil ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Nome da categoria"
+            textField.text = category?.name
+        }
+        let okAction = UIAlertAction(title: title, style: .default) { (_) in
+            let category = category ?? Category(context: self.context)
+            category.name = alert.textFields?.first?.text
+            do {
+                try self.context.save()
+                self.loadCategories()
+            } catch {
+                print(error)
+            }
+        }
+        alert.addAction(okAction)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     private func loadCategories() {
         let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
@@ -61,8 +84,61 @@ class CategoriesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let category = categories[indexPath.row]
+        
+        if selectedCategories.contains(category) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         cell.textLabel?.text = category.name
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let editAction = UIContextualAction(style: .normal, title: "Editar") { (action, view, completionHandler) in
+            let category = self.categories[indexPath.row]
+            self.showCategoryAlert(for: category)
+            completionHandler(true)
+        }
+        editAction.backgroundColor = .systemBlue
+        editAction.image = UIImage(systemName: "pencil")
+        
+        return UISwipeActionsConfiguration(actions: [editAction])
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Excluir") { (action, view, completionHandler) in
+            let category = self.categories[indexPath.row]
+            self.context.delete(category)
+            do {
+                try self.context.save()
+            } catch {
+                
+            }
+            self.categories.remove(at: indexPath.row)
+            self.selectedCategories.remove(category)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            completionHandler(true)
+        }
+        deleteAction.backgroundColor = .systemRed
+        deleteAction.image = UIImage(systemName: "trash")
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = self.categories[indexPath.row]
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell?.accessoryType == UITableViewCell.AccessoryType.none {
+            cell?.accessoryType = .checkmark
+            selectedCategories.insert(category)
+        } else {
+            cell?.accessoryType = .none
+            selectedCategories.remove(category)
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 
 }
