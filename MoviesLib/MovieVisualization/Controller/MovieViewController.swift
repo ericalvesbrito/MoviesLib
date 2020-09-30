8a0e48137c18e71f398af87d8731ae20c223bec3
Eric Brito <eric_brito@msn.com>
2020-09-29 23:05:21 -0300

    - Multimídia!

--- MoviesLib/MovieVisualization/Controller/MovieViewController.swift
+++ MoviesLib/MovieVisualization/Controller/MovieViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import AVKit
 
 final class MovieViewController: UIViewController {
     
     // MARK: - Properties
     var movie: Movie!
+    var moviePlayer: AVPlayer?
+    var moviePlayerController: AVPlayerViewController?
+    var trailer: String = ""
     
     // MARK: - IBOutlets
     @IBOutlet weak var imageViewPoster: UIImageView!
@@ -20,6 +24,7 @@ final class MovieViewController: UIViewController {
     @IBOutlet weak var labelDuration: UILabel!
     @IBOutlet weak var labelRating: UILabel!
     @IBOutlet weak var textViewSummary: UITextView!
+    @IBOutlet weak var viewTrailer: UIView!
     
     // MARK: - Super Methods
     override func viewDidLoad() {
@@ -34,6 +39,10 @@ final class MovieViewController: UIViewController {
         textViewSummary.text = movie.summary
         imageViewPoster.image = movie.poster
         labelCategories.text = (movie.categories as? Set<Category>)?.compactMap({$0.name}).sorted().joined(separator: " | ")
+        
+        if let title = movie.title {
+            loadTrailer(with: title)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -42,5 +51,50 @@ final class MovieViewController: UIViewController {
         }
     }
     
+    // MARK: - Methods {
+    private func loadTrailer(with title: String) {
+        let itunesPath = "https://itunes.apple.com/search?media=movie&entity=movie&term="
+        guard let encodedTitle = title.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+            let url = URL(string: "\(itunesPath)\(encodedTitle)") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let apiResult = try! JSONDecoder().decode(ItunesResult.self, from: data!)
+            self.trailer = apiResult.results.first?.previewUrl ?? ""
+            self.prepareVideo()
+        }.resume()
+    }
+    
     // MARK: - IBActions
+    @IBAction func playTrailer(_ sender: UIButton) {
+        /*
+        guard let moviePlayerController = moviePlayerController else {return}
+        present(moviePlayerController, animated: true) {
+            self.moviePlayer?.play()
+        }
+        */
+        viewTrailer.isHidden = false
+        sender.isHidden = true
+        moviePlayer?.play()
+    }
+    
+    private func prepareVideo() {
+        guard let url = URL(string: trailer) else {return}
+        moviePlayer = AVPlayer(url: url)
+        DispatchQueue.main.async {
+            self.moviePlayerController = AVPlayerViewController()
+            self.moviePlayerController?.player = self.moviePlayer
+            
+            guard let movieView = self.moviePlayerController?.view else {return}
+            movieView.frame = self.viewTrailer.bounds
+            self.viewTrailer.addSubview(movieView)
+        }
+    }
+}
+
+struct ItunesResult: Codable {
+    let results: [MovieInfo]
+}
+
+struct MovieInfo: Codable {
+    let previewUrl: String
 }
